e457b83fd5e8724edbe9830f94486183f6c80694
sawonhossain34 <sawonhossain3491@gmail.com>
2023-05-10 09:55:16 +0600

    about us section add done

---
+++ src/pages/Home/Services/Services.jsx
@@ -0,0 +1,15 @@
+
+
+const Services = () => {
+    return (
+        <div className="mt-4">
+            <div className="text-center"> 
+                <h3 className="text-2xl font-bold text-orange-500">serice</h3>
+                <h3 className="text-5xl">Our Service Area</h3>
+                <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </p>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
