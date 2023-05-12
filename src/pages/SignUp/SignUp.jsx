1118aee35ae1ca1cb7cf3ecab3757c8d4661e437
sawonhossain34 <sawonhossain3491@gmail.com>
2023-05-13 00:06:40 +0600

    jwt and useNavigate hook

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -48,7 +48,7 @@ const SignUp = () => {
                                 <label className="label">
                                     <span className="label-text">Conform Password</span>
                                 </label>
-                                <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
